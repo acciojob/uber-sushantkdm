8c46cbd938b71981c6e6abe6cd59e7eedd45d4c4
Sushant <sushantkadam936@gmail.com>
2023-02-25 15:02:51 +0530

    uber

--- src/main/java/com/driver/services/DriverService.java
+++ src/main/java/com/driver/services/DriverService.java
@@ -1,10 +1,11 @@
 package com.driver.services;
 
+
 import com.driver.model.Driver;
 
 public interface DriverService {
 
-		public void register(String mobile, String password);
-		public void removeDriver(int driverId);
-		public void updateStatus(int driverId);
+	public void register(String mobile, String password);
+	public void removeDriver(int driverId);
+	public void updateStatus(int driverId);
 }
